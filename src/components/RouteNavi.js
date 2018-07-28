f3b500b5015de42c7ba820a1b8d402b235ce1408
Angel <angelyuan37@gmail.com>
2018-07-29 07:03:13 +1000

    added path polyline to navigation

--- src/components/RouteNavi.js
+++ src/components/RouteNavi.js
@@ -56,8 +56,8 @@ class Route extends React.Component {
                 },
             },],
             region: {
-                latitude: -37.799265,
-                longitude: 144.963954,
+                latitude: -37.800365,
+                longitude: 144.965954,
                 latitudeDelta: 0.025,
                 longitudeDelta: 0.0221,
             },
@@ -81,15 +81,37 @@ class Route extends React.Component {
                     style={styles.container}
                     annotations={this.state.markers[0]}
                 > 
+                <Marker
+                    coordinate={ {latitude: -37.796265,longitude: 144.960954 }}
+                    pinColor = 'blue'
+                />
+                <Marker
+                    coordinate={ {latitude: -37.809944, longitude: 144.969788 }}
+                    pinColor = 'black'
+                />
                 <Polyline
                     coordinates={[
-                        { latitude: -37.809944,longitude: 144.969788 },
                         { latitude: -37.796265,longitude: 144.960954 },
+                        { latitude: -37.796566, longitude: 144.963322 },
+                        { latitude: -37.797786, longitude: 144.963236 },
+                        { latitude: -37.797888, longitude: 144.964331 },
+
+                        { latitude: -37.800363, longitude: 144.963923 },
+                        { latitude: -37.801109, longitude: 144.969996 },
+                        { latitude: -37.808117, longitude: 144.968779 },
+                        { latitude: -37.809944, longitude: 144.969788 },
                     ]}
                     strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
                     strokeColors={[
                         '#7F0000',
                         '#B24112',
+                        '#7F0000',
+                        '#7F0000',
+
+                        '#7F0000',
+                        '#7F0000',
+                        '#7F0000',
+                        '#7F0000',
                     ]}
                     strokeWidth={6}
                 />
