28e3f247636e1b4f6d8efecb875e0204687dcaa9
Angel <angelyuan37@gmail.com>
2018-07-28 18:21:48 +1000

    django init setup run "python manage.py runserver" to start localhost"

---
+++ backend/djangovenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/angelyuan/Desktop/codeB/walkingCompanions/backend/djangovenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
